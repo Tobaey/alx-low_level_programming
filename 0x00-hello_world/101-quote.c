758686c01d6f38228cb4ef49f2af9753fb5472bd
Tobaey <ademolatobie@gmail.com>
2022-07-04 12:02:09 -0700

    Commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,71 +1,14 @@
-nclude <unistd.h>
-
- 
-
- 
-
-
-
- 
-
- 
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- *
- *  
- *
- *  * main - prints a standard error
- *
- *   
- *
- *   *
- *
- *    
- *
- *    * Return: Always 1 (Success)
- *
- *     
- *
- *     */
-
- 
-
-
-
- 
-
- 
-
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
 int main(void)
-
-	 
-
 {
-
-	 
-
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-	 
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
-
-	 
-
 }
-
-
-
- 
-
-fprintf(stderr, "and that piece of art is useful\" - \
-
-		Dora Korpar, 2015-10-19\n");
-
- 
-
-return (1);
-
-}
-
-
